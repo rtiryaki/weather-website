eb8bf39c71d1c1ad93b13709dcf2553a0bc94dcc
rtiryaki <rtiryaki@gmail.com>
2020-06-01 21:09:02 +0300

    Remove unnecessary console.log command

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,3 @@
-console.log("Client side Javascript file...");
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
